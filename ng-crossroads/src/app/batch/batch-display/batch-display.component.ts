4f7efab1a1b954de9a9b2a9b81540d5067f81db9
ruchir <ruchir.mehta@gsa.gov>
2018-12-03 22:31:03 -0500

    added batch and evidence table details and backend API - 03Dec2018

--- ng-crossroads/src/app/batch/batch-display/batch-display.component.ts
+++ ng-crossroads/src/app/batch/batch-display/batch-display.component.ts
@@ -1,49 +1,95 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild} from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DashboardService } from '../../services/dashboard.service';
+import {MatPaginator, MatSort} from '@angular/material';
+
+export interface BatchElement {
+  batchId: any;
+  batchName: any;
+  employeeId: any;
+  evidenceCount: any;
+  expires: any;
+} 
+
+export interface EvidenceElement {
+  evidence: any;
+  evidence1B: any;
+  description: any;
+  status: any;
+  evidenceSubmissionId: any;
+  batchId: any;
+} 
 
 @Component({
   selector: 'app-batch-display',
   templateUrl: './batch-display.component.html',
   styleUrls: ['./batch-display.component.scss']
 })
-export class BatchDisplayComponent {
+export class BatchDisplayComponent implements OnInit {
 
-  public table = {
-    headers: [
-      { name: 'Batch ID' },
-      { name: 'Batch Name' },
-      { name: 'Evidence Counts' },
-      { name: 'Expires' }
-    ],
-    rows: [
-      {
-        id: 1,
-        batchName: 'Batch Name 01',
-        evidenceCounts: 100,
-        expires: Date.now(),
-        uri: [ 1 ]
-      },
-      {
-        id: 2,
-        batchName: 'Batch Name 01',
-        evidenceCounts: 100,
-        expires: Date.now(),
-        uri: [ 2 ]
-      },
-      {
-        id: 3,
-        batchName: 'Batch Name 01',
-        evidenceCounts: 100,
-        expires: Date.now(),
-        uri: [ 3 ]
-      },
-      {
-        id: 4,
-        batchName: 'Batch Name 01',
-        evidenceCounts: 100,
-        expires: Date.now(),
-        uri: [ 4 ]
+  dataSource : BatchElement[] = [];
+  dataSourceEvidence : EvidenceElement[] = [];
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  displayedColumns: string[] = ['batchId', 'batchName', 'employeeId', 'evidenceCount','expires'];
+  displayedColumnsEvidence: string[] = ['batchId', 'evidence', 'evidence1B', 'description','status','evidenceSubmissionId'];
+
+  constructor(private formBuilder: FormBuilder, private router: Router, private dashboardService: DashboardService) { }
+
+  public data = '{"data":[  {  "data":{  "name":"Andrew","gender":"Male"}, "children":[{  "data":{   "name":"Andrewson", "gender":"Male"  },"children":[  {  "data":{  "name":"Eric", "gender":"Male"}}        ]}]}]}';
+
+  ngOnInit() {
+    this.showDashboard('');
+  }
+
+  showDashboard(searchString?: string) {
+   // console.log("cookie getall ::: " + JSON.stringify(this.cookieService.getAll()));
+    this.dashboardService.getDashboardData('63718','2000','1','10','Name','ASC').subscribe((response) => {
+        console.log(response);
+        this.dataSource = this.getBatchData(response);
+        return response;
+    });
+
+  }
+
+  getBatchData(responseData:any) {
+    let resultsEmbedded = responseData["_embedded"];
+    if(resultsEmbedded) {
+      console.log("inside")
+      let results = resultsEmbedded["batchList"];
+      let resultArr : BatchElement[] = [] ;
+      for(let result of results) {
+          resultArr.push({batchId: result["batchId"], batchName: result["batchName"], employeeId: result["employeeId"], 
+          evidenceCount: result["evidenceCount"],expires: result["expires"]});
       }
-    ]
-  };
+      console.log(JSON.stringify(resultArr));
+      return resultArr;
+    }
+  }
 
+  getEvidence(row){
+    console.log(JSON.stringify(row["batchId"]));
+    this.dashboardService.getEvidenceData(row["batchId"]).subscribe((response) => {
+        console.log(response);
+        this.dataSourceEvidence = this.getEvidenceResponseData(response);
+        return response;
+    });
+  }
+
+  getEvidenceResponseData(responseData:any) {
+    let resultsEmbedded = responseData["_embedded"];
+    if(resultsEmbedded) {
+      console.log("inside")
+      let results = resultsEmbedded["evidenceList"];
+      let resultArr : EvidenceElement[] = [] ;
+      for(let result of results) {
+          resultArr.push({batchId: result["batchId"], evidence: result["evidence"], evidence1B: result["evidence1B"], 
+                          description: result["description"],status: result["status"],
+                          evidenceSubmissionId: result["evidenceSubmissionId"]});
+      }
+      console.log(JSON.stringify(resultArr));
+      return resultArr;
+    }
+  }
 }
