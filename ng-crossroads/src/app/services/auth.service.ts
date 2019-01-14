3901d6cf2e30f62ec86f3086708edc5653531b19
Colin Alford <colinalford@gmail.com>
2019-01-13 21:50:24 -0500

    Hooked up auth with API

--- ng-crossroads/src/app/services/auth.service.ts
+++ ng-crossroads/src/app/services/auth.service.ts
@@ -2,35 +2,65 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 
 import { User } from '../models/user';
 
 @Injectable()
 export class AuthService {
-  private BASE_URL = 'http://crossapi.us-west-1.elasticbeanstalk.com/crossroads/v1/login';
+  private BASE_URL = 'http://crossservice.us-west-1.elasticbeanstalk.com/crossroads/v1/login';
 
   constructor(private http: HttpClient) {}
 
   getToken(): string {
-    return localStorage.getItem('token');
+    try {
+      return localStorage.getItem('token');
+    } catch {
+      return undefined;
+    }
   }
 
   logIn(email: string, password: string): Observable<any> {
-    if (email !== 'lynne.johnson@octoconsulting.com' || password !== '1234') {
-      const url = `${this.BASE_URL}`;
-      return this.http.post<User>(url, {email: email, password: password});
-    } else {
-      const user: User = {
-        id: '63718',
-        email: 'lynne.johnson@octoconsulting.com',
-        token: '1234567890'
-      };
-      return of(user);
-    }
+
+    const url = `${this.BASE_URL}`;
+    const token = this.encodeAuthToken(email, password);
+    return this.http.post<User>(
+      url,
+      {},
+      {
+        headers: {
+          Authorization: `Basic ${token}`,
+        }
+      }
+    );
+    // .pipe(
+    //   catchError(response => {console.log(response); return of(response.error.text); })
+    // );
+
+    // if (email !== 'lynne.johnson@octoconsulting.com' || password !== '1234') {
+    //   const url = `${this.BASE_URL}`;
+    //   return this.http.post<User>(url, {
+    //     headers: {
+    //       Authorization: 'Basic c3R1ZGVudDk6T2N0bzEyMyFAIw=='
+    //     },
+    //     params: {email: email, password: password}
+    //   });
+    // } else {
+    //   const user: User = {
+    //     id: '63718',
+    //     email: 'lynne.johnson@octoconsulting.com',
+    //     token: '1234567890'
+    //   };
+    //   return of(user);
+    // }
   }
 
   getStatus(): Observable<User> {
     const url = `${this.BASE_URL}/status`;
     return this.http.get<User>(url);
+  }
+
+  encodeAuthToken(username: string, password: string): string {
+    return window.btoa(`${username}:${password}`);
   }
 }
--- ng-crossroads/src/app/store/effects/auth.effects.ts
+++ ng-crossroads/src/app/store/effects/auth.effects.ts
@@ -2,7 +2,7 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import { Observable, of } from 'rxjs';
+import { Observable, of, observable, throwError } from 'rxjs';
 import { map, switchMap, catchError, tap } from 'rxjs/operators';
 
 import { AuthService } from '../../services/auth.service';
@@ -11,7 +11,6 @@ import {
   LogIn,
   LogInSuccess,
   LogInFailure,
-  LogOut,
 } from '../actions/auth.actions';
 
 
@@ -32,8 +31,17 @@ export class AuthEffects {
       switchMap(payload => {
         return this.authService.logIn(payload.email, payload.password)
           .pipe(
+            // Temporary Fix Until Content-type is fixed on API
+            catchError((response) => {
+              return of(response);
+            }),
             map((user) => {
-              return new LogInSuccess({token: user.token, email: user.email, id: user.id});
+              const text = user.error.text;
+              if (text) {
+                return new LogInSuccess({token: text, email: payload.email, id: ''});
+              } else {
+                return new LogInFailure({ error: user.error });
+              }
             }),
             catchError((error) => {
               return of(new LogInFailure({ error: error }));
@@ -47,7 +55,7 @@ export class AuthEffects {
     ofType(AuthActionTypes.LOGIN_SUCCESS),
     tap((user) => {
       localStorage.setItem('token', user.payload.token);
-      localStorage.setItem('userId', user.payload.id);
+      localStorage.setItem('userId', user.payload.id || '');
       this.router.navigateByUrl('/batches');
     })
   );
